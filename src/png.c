8e340f86557986d7eeb24ddf51c22fda4771d6e1
Flambrew <matherne.a4@gmail.com>
2024-05-23 20:56:31 -0400

    gitignore kinda brokey, wrote png chunk reader

--- src/png.c
+++ src/png.c
@@ -1,17 +1,56 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
 
 #include "util.h"
 
+#define PNG_HEADER 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a
+
+typedef struct Chunk {
+    uint32_t length;
+    uint8_t type[4];
+    uint8_t *data;
+    uint32_t crc;
+} Chunk;
+
 bool pngVerify(FILE *file) {
-    int i, PNG_HEADER[] = {137, 80, 78, 71, 13, 10, 26, 10};
+    uint8_t i, header[8] = {PNG_HEADER};
     for (i = 0; i < 8; ++i) {
-        if (PNG_HEADER[i] != getc(file)) {
+        if (getc(file) != header[i]) {
             return false;
         }
     }
     return true;
+}
+
+Chunk nextChunk(FILE *file) {
+    Chunk out = {0, 0, 0, 0};
+
+    uint8_t i;
+    for (i = 0; i < 4; ++i) {
+        out.length <<= 8;
+        out.length |= getc(file);
+    }
+
+    for (i = 0; i < 4; ++i) {
+        out.type[i] = getc(file);
+    }
+
+    out.data = malloc(sizeof(uint8_t) * out.length);
+    for (i = 0; i < out.length; ++i) {
+        out.data[i] = getc(file);
+    }
+
+    for (i = 0; i < 4; ++i) {
+        out.crc = getc(file);
+    }
+    
+    return out;
+}
+
+void freeChunk(Chunk c) {
+    free(c.data);
 }
 
 RGB *read(char *path) {
